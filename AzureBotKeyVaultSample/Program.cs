97ed2a72e96b34f4a072822ab442d1e2fdf4eeab
nayden <767336@cognizant.com>
2020-01-17 22:14:14 -0300

    Add Files

---
+++ AzureBotKeyVaultSample/Program.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.6.2
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+namespace AzureBotKeyVaultSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        //Here inside the webhost I add the reference to the keyvault with the configuration class
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+             WebHost.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((context, config) =>
+        {
+            var root = config.Build();
+            var vault = root["KeyVault:Vault"];
+            var clientId = root["KeyVault:ClientId"];
+            var secret = root["KeyVault:ClientSecret"];
+
+            if (!string.IsNullOrEmpty(vault))
+            {
+              config.AddAzureKeyVault(vault, clientId, secret);
+            }
+
+
+        })
+        .UseStartup<Startup>();
+    }
+}
+
---
+++ AzureBotKeyVaultSample/Startup.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.6.2
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using AzureBotKeyVaultSample.Bots;
+
+namespace AzureBotKeyVaultSample
+{
+    public class Startup
+    {
+        public string MicrosoftAppId;
+        public string MicrosoftAppPassword;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            // Here I take all the secrets of azure key vault services.
+
+            MicrosoftAppId = Configuration["MicrosoftAppId"].ToString();
+            MicrosoftAppPassword = Configuration["MicrosoftAppPassword"].ToString();
+            //All secrets are available from the configuration class.
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Create the Bot Framework Adapter with error handling enabled.
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddTransient<IBot, EchoBot>();
+
+        
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseWebSockets();
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
